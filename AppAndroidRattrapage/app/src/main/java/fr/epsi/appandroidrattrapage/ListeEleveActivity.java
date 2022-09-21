ea84b517d55017bc2b34906fed43c112535b99aa
Christophe <guide.christophe@live.fr>
2022-09-21 14:59:13 +0200

    update app android (ShowRattrapage)

---
+++ AppAndroidRattrapage/app/src/main/java/fr/epsi/appandroidrattrapage/ListeEleveActivity.java
@@ -0,0 +1,14 @@
+package fr.epsi.appandroidrattrapage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListeEleveActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_liste_eleve);
+    }
+}
--- AppAndroidRattrapage/app/src/main/java/fr/epsi/appandroidrattrapage/ShowRattrapageActivity.java
+++ AppAndroidRattrapage/app/src/main/java/fr/epsi/appandroidrattrapage/ShowRattrapageActivity.java
@@ -2,7 +2,9 @@ package fr.epsi.appandroidrattrapage;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class ShowRattrapageActivity extends AppCompatActivity {
 
@@ -11,4 +13,19 @@ public class ShowRattrapageActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_rattrapage);
     }
+
+    public void clickButtonEleve(View v){
+        Intent navigationToListEleve = new Intent(this, ListeEleveActivity.class);
+        //navigationToListEleve.putExtra("listEleve", listeEleve);
+        startActivity(navigationToListEleve);
+    }
+
+    public void clickButtonChrono(View v){
+
+    }
+
+    public void clickButtonSujet(View v){
+
+    }
+
 }
